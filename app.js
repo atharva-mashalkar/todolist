b12013aa78181386fc94c68859c7af7367fd9486
Atharva Mashalkar <vandanamashalkar12@gmail.com>
2019-07-02 23:14:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var todoController = require('./controllers/todoController.ejs');
+var app = express();
+
+// set template view
+app.set('view engine' , 'ejs');
+
+// static files
+app.use(express.static('./public'));
+
+// fire controller
+todoController(app);
+
+//listen to port
+app.listen(3000);
+console.log('You are listening to port 3000');
